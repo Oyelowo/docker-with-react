47b94eb1a668f601242afe4736f5d1519a323e2a
Oyelowo Oyedayo <oyelowooyedayo@gmail.com>
2020-07-23 22:30:24 +0300

    Rebuild

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>We changed this</h1>
+        <h1>We changed this should work</h1>
       </header>
     </div>
   );
